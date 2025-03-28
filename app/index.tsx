e2708510f5fbad915c2e5fc8a6425cd29f81ead2
alexengqc <alexengqc@gmail.com>
2025-03-29 06:55:59 +0800

    initial

---
+++ app/index.tsx
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+import Icon from '@/assets/images/wordle-icon.svg';
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>sdsdsd</Text>
+    </View>
+  );
+}
