a68140ba7a778c08b41c5589f6153f4f502a3288
alexengqc <alexengqc@gmail.com>
2025-03-31 00:40:28 +0800

    Revert "Fixes"
    This reverts commit 350bc7fe7b774d5771b5249f4f56afef557d555b.

--- components/OnscreenKeyboard.tsx
+++ components/OnscreenKeyboard.tsx
@@ -14,9 +14,9 @@ export const ENTER = 'ENTER';
 export const BACKSPACE = 'BACKSPACE';
 
 const keys = [
-    ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
-    ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'],
-    [ENTER, 'Z', 'X', 'C', 'V', 'B', 'N', 'M', BACKSPACE],
+    ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
+    ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'],
+    [ENTER, 'z', 'x', 'c', 'v', 'b', 'n', 'm', BACKSPACE],
 ];
 
   const OnScreenKeyboard = ({
